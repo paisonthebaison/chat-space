be5eba58f2b12393f0727832f8f18ec4474f6908
paisonthebaison <paisonthebaison@gmail.com>
2020-01-13 14:55:54 +0900

    hidouki

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(function(){ 
+  function buildHTML(message){
+   if ( message.image ) {
+     var html =
+     `<div class="main-chat__middle__message" ${message.id}>
+     <div class="main-chat-middle__message__upper-info">
+     <p class="upper-info__talker">${message.user_name}</p>
+     <p class="upper-info__date">${message.created_at}</p>
+     </div>
+     <p class="message__text">
+     ${message.content}
+     <img class="lower-message__image" src=${message.image}>
+     </p>
+     </div>`
+     return html;
+   } else {
+     var html =
+     `<div class="main-chat__middle__message" ${message.id}>
+     <div class="main-chat-middle__message__upper-info">
+     <p class="upper-info__talker">${message.user_name}</p>
+     <p class="upper-info__date">${message.created_at}</p>
+     </div>
+     <p class="message__text">
+     ${message.content}
+     </p>
+     </div>`
+     return html;
+   };
+ }
+$('#new_message').on('submit', function(e){
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+   url: url,
+   type: "POST",
+   data: formData,
+   dataType: 'json',
+   processData: false,
+   contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+  })
+})
+});
