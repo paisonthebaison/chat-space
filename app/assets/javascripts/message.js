81ab589ffb69c82164201a1d0031c0b797a887b6
paisonthebaison <paisonthebaison@gmail.com>
2020-01-18 20:43:04 +0900

    automatic-update-conplete

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-     `<div class="main-chat__middle__message" ${message.id}>
+     `<div class="main-chat__middle__message" data-message-id=${message.id}>
         <div class="main-chat-middle__message__upper-info">
           <p class="upper-info__talker">${message.user_name}</p>
           <p class="upper-info__date">${message.created_at}</p>
@@ -15,7 +15,7 @@ $(function(){
      return html;
    } else {
      var html =
-     `<div class="main-chat__middle__message" ${message.id}>
+     `<div class="main-chat__middle__message" data-message-id=${message.id}>
         <div class="main-chat-middle__message__upper-info">
           <p class="upper-info__talker">${message.user_name}</p>
           <p class="upper-info__date">${message.created_at}</p>
@@ -26,7 +26,7 @@ $(function(){
      </div>`
      return html;
    };
- }
+ };
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
@@ -49,10 +49,14 @@ $('#new_message').on('submit', function(e){
   })
   .fail(function() {
     alert("メッセージ送信に失敗しました");
+    $('.new-massage__submit-btn').prop('disabled', false);
   });
 })
   var reloadMessages = function() {
-  last_message_id = $('.message:last').data("message-id");
+  last_message_id = $('.main-chat__middle__message:last').data("message-id");
+  console.log(last_message_id);
+  console.log($('.message:last'));
+  console.log($('.message:last')[0]);
   $.ajax({
     url: "api/messages",
     type: 'get',
@@ -65,17 +69,21 @@ $('#new_message').on('submit', function(e){
     $.each(messages, function(i, message) {
       insertHTML += buildHTML(message)
     });
-    $('.messages').append(insertHTML);
-    $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    $('.main-chat__middle').append(insertHTML);
+    $('.main-chat__middle').animate({ scrollTop: $('.main-chat__middle')[0].scrollHeight});
     $("#new_message")[0].reset();
     $(".form__submit").prop("disabled", false);
     }
   })
   .fail(function() {
+    
   });
+  
+  
 };
     if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-      setInterval(reloadMessages, 7000);
+      setInterval(reloadMessages, 1000);
     }
+    
 });
 
